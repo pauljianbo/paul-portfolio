8d127c9ed95f1998d97b83127bc4562eec324eac
pauljianbo <paul@greywolf.sg>
2024-12-11 11:41:04 +0800

    add skill section

---
+++ app/components/HomeCards/SkillSection.tsx
@@ -0,0 +1,15 @@
+
+const SkillSection = () => {
+  return (
+    <div className="max-w-7xl mx-auto bg-light-paper rounded-md p-4">
+      <h1 className="text-2xl font-bold text-center">Skills</h1>
+      <p className="text-center text-[16px] text-light-text-secondary w-3/4 mx-auto leading-[24px] mt-[20px]">
+        I am a software engineer with a passion for creating beautiful and functional web applications. I have a strong background in front-end development, but I am also comfortable with back-end development. I am a quick learner and I am always looking to expand my skills.
+      </p>
+
+
+    </div>
+  );
+};
+
+export default SkillSection;
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@
 import dynamic from 'next/dynamic';
 // Dynamically import the Banner component with no SSR
 const Banner = dynamic(() => import('./components/HomeCards/AnimatedCubeCard'), { ssr: false });
+import SkillSection from './components/HomeCards/SkillSection';
 
 const Home = () => {
   return (
     <div className="">
       <Banner />
-     
+      <SkillSection />
     </div>
   );
 }
