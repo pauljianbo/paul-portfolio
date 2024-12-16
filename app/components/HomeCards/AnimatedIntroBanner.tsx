b29df794e0aa10f832400c8546fe0daef11aff77
pauljianbo <paul@greywolf.sg>
2024-12-16 09:27:53 +0800

    testing animation

--- app/components/HomeCards/AnimatedIntroBanner.tsx
+++ app/components/HomeCards/AnimatedIntroBanner.tsx
@@ -1,19 +1,16 @@
 'use client';
-// import AnimatedCube from '../tools/Animation/3D/AnimatedCube';
 import IntroText from '../tools/Animation/IntroText';
-import AnimatedCharacter from '../tools/Animation/3D/AnimatedCharacter';
+// import AnimatedCharacter from '../tools/Animation/3D/AnimatedCharacter';
 const AnimatedIntroBanner = () => {
   return (
     <div className="mx-auto mt-20 flex max-w-7xl md:mt-0 md:h-screen">
       <div className="aspect-square w-full md:w-1/2">
         <IntroText />
       </div>
-      <div className="hidden aspect-square w-1/2 translate-y-[40px] items-center md:flex">
+      {/* <div className="hidden aspect-square w-1/2 translate-y-[40px] items-center md:flex">
         <AnimatedCharacter />
-      </div>
-      {/* <div className="aspect-square w-1/2">
-        <AnimatedCube />
       </div> */}
+    
     </div>
   );
 };
